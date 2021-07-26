4ef64c7c1f1a3d95aeef3deae97d156acc31cc77
Joao Caio <jocaio@uol.com.br>
2021-07-26 17:56:09 -0300

    Primeiro commit aula Jogo da Velha

---
+++ script.js
@@ -0,0 +1,57 @@
+// Initial Data
+let square = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: ''    
+};
+let player = '';
+let warning = '';
+let playing = false;
+
+reset();
+
+// Events
+document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick);
+});
+
+// Functions
+function itemClick() {
+    console.log ( event.target )
+}
+
+
+function reset() {
+    warning = '';
+
+    let random = Math.floor(Math.random() *2);
+    player = (random === 0) ? 'x' : 'o';
+    /* if (rendom ===0) {
+        player = 'x';
+    } else {
+        player = 'o';
+    } */ 
+
+    for(let i in square) {
+        square[i] = '';
+    }
+
+    playing = true;
+
+    renderSquare();
+    renderInfo();
+}
+
+function renderSquare() {
+    for(let i in square) {
+        let item = document.querySelector(`div[data-item=${i}]`);
+        item.innerHTML = square[i];
+    }
+}
+
+function renderInfo() {
+    document.querySelector('.vez').innerHTML = player; 
+    document.querySelector('.resultado').innerHTML = warning;
+
+}
