9131554a105846400f57f9a5b602fd0a43b71ca3
João Caio Lopes Bernardino <jocaio@uol.com.br>
2021-07-27 08:38:37 -0300

    Finalizando Jogo da Velha Mogi

--- script.js
+++ script.js
@@ -17,10 +17,14 @@ document.querySelectorAll('.item').forEach(item => {
 });
 
 // Functions
-function itemClick() {
-    console.log ( event.target )
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item');
+    if(playing && square[item] === '' ) {
+        square[item] = player;
+        renderSquare();
+        togglePlayer();
+    }
 }
-
 
 function reset() {
     warning = '';
@@ -48,10 +52,66 @@ function renderSquare() {
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
     }
+
+    checkGame();
 }
 
 function renderInfo() {
     document.querySelector('.vez').innerHTML = player; 
     document.querySelector('.resultado').innerHTML = warning;
 
+}
+
+function togglePlayer() {
+    player = (player === 'x') ? 'o' : 'x';
+    renderInfo();
+}
+
+function checkGame() {
+    if(checkWinnerFor('x')) {
+        warning = 'O "x" venceu';
+        playing = false;
+    } else if (checkWinnerFor('o')) {
+        warning = 'O "o" venceu';
+        playing = false;
+    } else if (isFull()) {
+        warning = 'Empatou';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(player) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos) {
+        let pArray = pos[w].split(','); // a1, a2, a3
+        let hasWon = pArray.every(option => square[option] === player);
+        if(hasWon) {
+            return true; 
+        }
+    }
+
+    return false;
+
+}
+
+function isFull() {
+    for(let i in square) {
+        if(square[i] === '') {
+            return false;
+        }
+    }
+
+    return true;
 }
